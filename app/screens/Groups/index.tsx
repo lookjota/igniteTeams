302213f9f1ef190afdbf32fcb6571293269fd6bc
lookjota <lookjota@gmail.com>
2024-08-24 15:09:38 -0300

    Criando componente Header

--- app/screens/Groups/index.tsx
+++ app/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
 
+import { Header } from "@/app/components/Headers";
 import { Container } from "./styles";
 
 export function Groups() {
   return (
     <Container>
-
+      <Header/>
 
 
     </Container>
